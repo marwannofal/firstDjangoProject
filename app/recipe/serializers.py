8685d16fa97d50b8d2540dc774614f0e1d1ef22f
marwannofal <marwannofal64@gmail.com>
2024-11-19 10:09:05 +0300

    finishing the whole recipe APIs

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -9,3 +9,10 @@ class RecipeSerializer(serializers.ModelSerializer):
     class Meta:
         model = Recipe
         fields = ['id', 'title', 'time_minutes', 'price', 'link']
+        read_only_fields = ['id']
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serializer for Recipe Detail View."""
+
+    class Meta(RecipeSerializer.Meta):
+        fields = RecipeSerializer.Meta.fields + ['description']
